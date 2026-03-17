5859624f3e2952b2ee7c7c3ea06b516c6fb176f1
Luis Hernandez <luis.hernandez9@stryker.com>
2026-03-17 11:29:32 -0500

    pipeline fixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "api" {
